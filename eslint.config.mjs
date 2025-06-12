3f2c6ebca1968b097eb2f5b11ccd90b6f12e7af5
박승희 <ridia05031@gmail.com>
2025-06-12 13:31:51 +0900

    [no-issue] fix: remove storybook eslint config to resolve typescript path resolution conflict

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,6 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   eslintConfigPrettier,
-  ...storybook.configs["flat/recommended"],
 ];
 
 export default eslintConfig;
