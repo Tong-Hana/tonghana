cee72cb97210045f30a2cf518d0c9e793de28bfc
박승희 <ridia05031@gmail.com>
2025-06-12 17:45:48 +0900

    [#15] feat: Input 컴포넌트 스토리 파일 추가

---
+++ src/components/common/input/Input.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Input from "@/components/common/input/Input";
+
+const meta: Meta<typeof Input> = {
+  title: "Common/Input",
+  component: Input,
+  tags: ["autodocs"],
+  argTypes: {
+    placeholder: { control: "text" },
+    fullWidth: { control: "boolean" },
+    variant: {
+      control: "radio",
+      options: ["outlined", "standard"],
+    },
+    type: {
+      control: "select",
+      options: ["text", "number", "email", "password"],
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Input>;
+
+export const Default: Story = {
+  args: {
+    placeholder: "메세지를 입력하세요.",
+    fullWidth: true,
+    variant: "outlined",
+  },
+};
+
+export const WithCustomWidth: Story = {
+  args: {
+    placeholder: "1천만원",
+    fullWidth: false,
+    variant: "outlined",
+    className: "w-60",
+    inputClassName: "text-lg font-bold",
+  },
+};
+
+export const PasswordField: Story = {
+  args: {
+    placeholder: "비밀번호",
+    type: "password",
+    fullWidth: false,
+    className: "w-72",
+  },
+};
