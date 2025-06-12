190003eedc59d12a93e19a8f5c2028a0092c32fe
jhpark0888 <jhpark0888@naver.com>
2025-06-12 16:29:26 +0900

    [#12] style: 별돌이, 별순이 이미지 추가

---
+++ src/assets/assets.tsx
@@ -0,0 +1,36 @@
+import HomeIconOutline from "@/assets/icons/bottom_nav/nav_home_icon.svg";
+import HomeIconFilled from "@/assets/icons/bottom_nav/filled_nav_home_icon.svg";
+
+import FolderIconOutline from "@/assets/icons/bottom_nav/nav_folder_icon.svg";
+import FolderIconFilled from "@/assets/icons/bottom_nav/fiiled_nav_folder_icon.svg";
+
+import HeartIconOutline from "@/assets/icons/bottom_nav/nav_heart_icon.svg";
+import HeartIconFilled from "@/assets/icons/bottom_nav/filled_nav_heart_icon.svg";
+
+import ChatIconOutline from "@/assets/icons/bottom_nav/nav_chat_icon.svg";
+import ChatIconFilled from "@/assets/icons/bottom_nav/filled_nav_chat_icon.svg";
+
+import ProfileIconOutline from "@/assets/icons/bottom_nav/nav_profile_icon.svg";
+import ProfileIconFilled from "@/assets/icons/bottom_nav/filled_nav_profile_icon.svg";
+
+import AttentionStarBoy from "@/assets/character/attention_starboy.svg";
+import AttentionStarGirl from "@/assets/character/attention_stargirl.svg";
+import InfoStarBoy from "@/assets/character/info_starboy.svg";
+import ThumbsUpStarGirl from "@/assets/character/thumbs_up_stargirl.svg";
+
+export {
+  HomeIconOutline,
+  HomeIconFilled,
+  FolderIconOutline,
+  FolderIconFilled,
+  HeartIconOutline,
+  HeartIconFilled,
+  ChatIconOutline,
+  ChatIconFilled,
+  ProfileIconOutline,
+  ProfileIconFilled,
+  AttentionStarBoy,
+  AttentionStarGirl,
+  InfoStarBoy,
+  ThumbsUpStarGirl,
+};
