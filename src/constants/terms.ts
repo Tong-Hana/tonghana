e75db93d99fff379a6f9b636624ac6fe15494c8f
jhpark0888 <jhpark0888@naver.com>
2025-06-14 22:35:46 +0900

    [#29] feat: LabelWithCheckBox, TermsAgreementGroup 컴포넌트 추가

---
+++ src/constants/terms.ts
@@ -0,0 +1,5 @@
+export const TERMS = [
+  { id: "termsOfService", label: "서비스 이용약관 동의 (필수)" },
+  { id: "privacyPolicy", label: "개인정보 처리방침 동의 (필수)" },
+  { id: "myData", label: "마이데이터 활용 동의 (필수)" },
+];
