be7e19f2e1b56483f8f514efa28aca57ab5bccf6
youlimsongs <giuhki@gmail.com>
2025-06-15 04:09:10 +0900

    [#18] feat: 프로필 컴포넌트 동적 데이터로 할당

--- src/app/(navbar)/home/page.tsx
+++ src/app/(navbar)/home/page.tsx
@@ -2,20 +2,43 @@
 import ProfileCard from "@/components/profile/ProfileCard";
 
 export default function HomePage() {
+  const user = {
+    name: "김하나",
+    age: 30,
+    job: "회사원",
+    location: "경기도 성남시",
+    description: "성남에 살고 서울에서 일해요 😊",
+    imageUrl: "/jennie.jpg",
+    target: "5년 안에 내집마련!",
+    totalAsset: "5억",
+    carCost: "5천만원",
+    houseCost: "3억",
+    portfolioValues: [300, 200, 165, 100, 0, 0, 0, 100],
+    debtPercent: "200%",
+    investorType: "적극투자",
+    portfolioType: "안정형",
+    showDetail: false,
+  };
+
   return (
     <div className="frame-container space-y-8">
       <div className="flex flex-col">
         <ProfileCard
-          name="별돌이"
-          age={25}
-          job="아티스트"
-          location="서울시 서초구"
-          description="서초에 살고 판교에서 일해요 😊"
-          imageUrl="/jennie.jpg" // public 폴더에 있는 임시 이미지
-          target="3년 안에 1억 모으기!"
-          totalAsset="30억"
-          carCost="1억"
-          houseCost="10억"
+          name={user.name}
+          age={user.age}
+          job={user.job}
+          location={user.location}
+          description={user.description}
+          imageUrl={user.imageUrl}
+          target={user.target}
+          totalAsset={user.totalAsset}
+          carCost={user.carCost}
+          houseCost={user.houseCost}
+          portfolioValues={user.portfolioValues}
+          debtPercent={user.debtPercent}
+          investorType={user.investorType}
+          portfolioType={user.portfolioType}
+          showDetail={user.showDetail}
         />
       </div>
     </div>
