d01f9e0a6855a17ea14bdb0a0f8547b491b081bc
zzimnii <happykeil@naver.com>
2025-06-15 19:18:55 +0900

    [#38] ci: EC2 배포 테스트 및 스크립트 작성

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,14 +1,23 @@
-"use client";
+// "use client";
 
-import "swagger-ui-react/swagger-ui.css";
-import dynamic from "next/dynamic";
+// import "swagger-ui-react/swagger-ui.css";
+// import dynamic from "next/dynamic";
 
-const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
+// const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
+
+// export default function DocsPage() {
+//   return (
+//     <div className="swagger-ui">
+//       <SwaggerUI url="/api/swagger" />
+//     </div>
+//   );
+// }
 
 export default function DocsPage() {
   return (
-    <div className="swagger-ui">
-      <SwaggerUI url="/api/swagger" />
+    <div>
+      <h1>API 문서</h1>
+      {/* Swagger UI 등 내용 */}
     </div>
   );
 }
