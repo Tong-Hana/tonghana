1e0b70c4b37a5dfc5c49f8f7145ec1e666c62665
박승희 <ridia05031@gmail.com>
2025-06-15 21:03:00 +0900

    [no-issue] feat: swagger 문서 뷰어에서 swagger-ui-react 제거 후 swagger-ui-dist 방식으로 변경

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import "swagger-ui-react/swagger-ui.css";
-import dynamic from "next/dynamic";
-
-const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
+import "swagger-ui-dist/swagger-ui.css";
 
 export default function DocsPage() {
   return (
-    <div className="swagger-ui">
-      <SwaggerUI url="/api/swagger" />
-    </div>
+    <iframe
+      src="/docs/swagger-ui/index.html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+      }}
+      title="Swagger Docs"
+    />
   );
 }
