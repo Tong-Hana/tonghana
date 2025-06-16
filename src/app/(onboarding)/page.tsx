4f94b0a41e34ebb008e300077a1dd7a2955d113f
박승희 <ridia05031@gmail.com>
2025-06-16 14:00:18 +0900

    [#53] refactor: 온보딩 인트로 페이지 onboarding 폴더 하위로 이동

--- src/app/(onboarding)/page.tsx
+++ src/app/(onboarding)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex">
       <p className="text-2xl text-hanagreen-normal-active font-semibold">
-        커스텀 컬러 적용완료
+        온보딩 인트로 페이지(초기진입)
       </p>
     </div>
   );
