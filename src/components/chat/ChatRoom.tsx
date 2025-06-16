b914481e73ff20db36b7aec7f7001cac1c59869e
jhpark0888 <jhpark0888@naver.com>
2025-06-16 11:12:37 +0900

    [#50] feat: 채팅방 목록 컴포넌트 구현

---
+++ src/components/chat/ChatRoom.tsx
@@ -0,0 +1,38 @@
+import { formatSmartDate } from "@/utils/dateformatter";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+  roomId: number;
+  imageUrl: string;
+  nickname: string;
+  lastMessage: string;
+  lastMessageDate: Date;
+};
+
+export default function ChatRoom({
+  roomId,
+  imageUrl,
+  nickname,
+  lastMessage,
+  lastMessageDate,
+}: Props) {
+  return (
+    <Link href={`/chat/${roomId}`}>
+      <div className="flex px-5 gap-2 items-center hover:bg-hanagreen-light">
+        <div className="w-[50px] h-[50px] relative rounded-full overflow-hidden">
+          <Image src={imageUrl} alt="profile" fill className="object-cover" />
+        </div>
+        <div className="py-4 flex flex-1 flex-col gap-1 text-text-primary border-b border-b-hanagreen-light-active">
+          <div className="flex items-center gap-2">
+            <p className="text-sm font-medium flex-1">{nickname}</p>
+            <p className="text-[10px] font-light text-text-secondary">
+              {formatSmartDate(lastMessageDate)}
+            </p>
+          </div>
+          <p className="text-xs font-light">{lastMessage}</p>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/utils/dateformatter.ts
+++ src/utils/dateformatter.ts
@@ -5,3 +5,33 @@ export function formatDashYYYYMMDD(date: Date): string {
 export function formatYYYYMMDD(date: Date): string {
   return formatDashYYYYMMDD(date).replaceAll("-", ""); // '20250530'
 }
+
+export function formatSmartDate(date: Date): string {
+  const now = new Date();
+
+  const pad = (n: number) => n.toString().padStart(2, "0");
+
+  const isToday =
+    date.getFullYear() === now.getFullYear() &&
+    date.getMonth() === now.getMonth() &&
+    date.getDate() === now.getDate();
+
+  if (isToday) {
+    const hours = pad(date.getHours());
+    const minutes = pad(date.getMinutes());
+    return `${hours}:${minutes}`;
+  }
+
+  const isThisYear = date.getFullYear() === now.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  if (isThisYear) {
+    return `${month}월 ${day}일`;
+  } else {
+    const year = date.getFullYear();
+    const padMonth = pad(month);
+    const padDay = pad(day);
+    return `${year}.${padMonth}.${padDay}`;
+  }
+}
