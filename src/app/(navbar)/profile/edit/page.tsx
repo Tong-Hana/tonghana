3ca9d3c1fc9833b90e7fc4677a3ea36f3b765320
박승희 <ridia05031@gmail.com>
2025-06-17 17:44:39 +0900

    [#52] feat: 내 정보 수정하기 페이지 구현

--- src/app/(navbar)/profile/edit/page.tsx
+++ src/app/(navbar)/profile/edit/page.tsx
@@ -1,8 +1,13 @@
 import Header from "@/components/common/Header";
+import ProfileEditForm from "@/components/profile/ProfileEditForm";
+
 export default function EditProfilePage() {
   return (
-    <div>
+    <div className="min-h-screen bg-background flex flex-col">
       <Header title="내 정보 수정하기" />
+      <div className="flex-1 overflow-y-auto pt-4 pb-6">
+        <ProfileEditForm />
+      </div>
     </div>
   );
 }
