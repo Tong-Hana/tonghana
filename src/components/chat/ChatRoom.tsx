4974235c2ad80fb900007055c026d5389d0ae926
jhpark0888 <jhpark0888@naver.com>
2025-06-17 16:39:40 +0900

    [#50] feat: 채팅 프로필 이미지 컴포넌트 분리

--- src/components/chat/ChatRoom.tsx
+++ src/components/chat/ChatRoom.tsx
@@ -1,6 +1,6 @@
 import { formatSmartDate } from "@/utils/dateformatter";
-import Image from "next/image";
 import Link from "next/link";
+import ChatProfileImage from "./ChatProfileImage";
 
 type Props = {
   roomId: number;
@@ -20,9 +20,7 @@ export default function ChatRoom({
   return (
     <Link href={`/chatroom/${roomId}`}>
       <div className="flex px-5 gap-2 items-center hover:bg-hanagreen-light">
-        <div className="w-[50px] h-[50px] relative rounded-full overflow-hidden">
-          <Image src={imageUrl} alt="profile" fill className="object-cover" />
-        </div>
+        <ChatProfileImage imageUrl={imageUrl} size={50} />
         <div className="py-4 flex flex-1 flex-col gap-1 text-text-primary border-b border-b-hanagreen-light-active">
           <div className="flex items-center gap-2">
             <p className="text-sm font-medium flex-1">{nickname}</p>
