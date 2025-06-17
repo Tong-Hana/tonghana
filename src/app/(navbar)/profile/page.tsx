721e2830c8fcd02fe37c3644430013706aff4eeb
youlimsongs <giuhki@gmail.com>
2025-06-17 10:08:15 +0900

    [#51] feat: mypage 구현

--- src/app/(navbar)/profile/page.tsx
+++ src/app/(navbar)/profile/page.tsx
@@ -2,7 +2,11 @@
 import DoughnutChart from "@/components/chart/DoughnutChart";
 import MonthlySpendingChart from "@/components/chart/MonthlySpendingChart";
 import Button from "@/components/common/button/Button";
+import DialogButton from "@/components/common/button/DialogButton";
+import { DislikeButton } from "@/components/common/button/ReactionButton";
+import RightArrow from "@/assets/icons/right_arrow_icon.svg";
 import ImageUploader from "@/components/profile/imageUploader/ImageUploader";
+import ProfileCardDetail from "@/components/profile/ProfileCardDetail";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -43,47 +47,109 @@ export default function MyPage() {
     { label: "기타", value: consumeHistoryData.other_rate * 100 },
   ];
 
-  // const pairingAnswerData = {
-  //   id: 1,
-  //   user_id: 1,
-  //   car_budget: 50000000,
-  //   date_budget: 300000,
-  //   shoes_budget: 200000,
-  //   preferred_city: "서울시",
-  //   preferred_district: "압구정동",
-  //   ideal_income_range: "1000만원대 이상",
-  //   created_at: "2023-01-01T10:00:00Z",
-  // };
-  // const answer = [
-  //   {
-  //     id: 1,
-  //     answer: `${(pairingAnswerData.car_budget / 10000).toLocaleString()}만원, ${(pairingAnswerData.date_budget / 10000).toLocaleString()}만원, ${(pairingAnswerData.shoes_budget / 10000).toLocaleString()}만원`,
-  //   },
-  //   {
-  //     id: 2,
-  //     answer: `${pairingAnswerData.preferred_city} ${pairingAnswerData.preferred_district}`,
-  //   },
-  //   {
-  //     id: 3,
-  //     answer: `${pairingAnswerData.ideal_income_range}`,
-  //   },
-  // ];
+  const pairingAnswerData = {
+    id: 1,
+    user_id: 1,
+    car_budget: 50000000,
+    date_budget: 300000,
+    shoes_budget: 200000,
+    preferred_city: "서울시",
+    preferred_district: "압구정동",
+    ideal_income_range: "1000만원대 이상",
+    created_at: "2023-01-01T10:00:00Z",
+  };
+  const answer = [
+    {
+      id: 1,
+      answer: `${(pairingAnswerData.car_budget / 10000).toLocaleString()}만원, ${(pairingAnswerData.date_budget / 10000).toLocaleString()}만원, ${(pairingAnswerData.shoes_budget / 10000).toLocaleString()}만원`,
+    },
+    {
+      id: 2,
+      answer: `${pairingAnswerData.preferred_city} ${pairingAnswerData.preferred_district}`,
+    },
+    {
+      id: 3,
+      answer: `${pairingAnswerData.ideal_income_range}`,
+    },
+  ];
 
   const router = useRouter();
 
-  const menuItems = [
-    { name: "내 정보 수정하기", path: "/profile/edit" },
-    { name: "FTTI 설문 다시하기", path: "/profile/ftti" },
-    { name: "로그아웃", path: "/login" },
-    { name: "회원탈퇴", path: "/login" },
+  const [showCard, setShowCard] = useState(false);
+
+  const [activeDialog, setActiveDialog] = useState<string | null>(null);
+
+  type MenuItem =
+    | {
+        name: string;
+        type: "link";
+        path: string;
+      }
+    | {
+        name: string;
+        type: "modal";
+        modalProps: {
+          title: string;
+          content: string;
+          onAction: () => void;
+        };
+      };
+
+  const menuItems: MenuItem[] = [
+    {
+      name: "내 정보 수정하기",
+      type: "link",
+      path: "/profile/edit",
+    },
+    {
+      name: "FTTI 설문 다시하기",
+      type: "link",
+      path: "/profile/ftti",
+    },
+    {
+      name: "로그아웃",
+      type: "modal",
+      modalProps: {
+        title: "로그아웃 하시겠어요?",
+        content: "다시 로그인할 수 있습니다.",
+        onAction: () => {
+          console.log("로그아웃 처리");
+          router.push("/login");
+        },
+      },
+    },
+    {
+      name: "회원탈퇴",
+      type: "modal",
+      modalProps: {
+        title: "탈퇴 하시겠어요?",
+        content: "모든 기록이 삭제됩니다.",
+        onAction: () => {
+          console.log("탈퇴 처리");
+          router.push("/login");
+        },
+      },
+    },
   ];
 
-  const [setShowCard] = useState(false);
+  // const handleLogoutAction = () => {
+  //   // 실제 로그아웃 처리 로직 (API 호출 등)
+  //   console.log("로그아웃 처리 실행!");
+  //   router.push("/login"); // 예시: 로그인 페이지로 이동
+  // };
+
+  // const handleWithdrawAction = () => {
+  //   // 실제 회원탈퇴 처리 로직 (API 호출 등)
+  //   console.log("회원탈퇴 처리 실행!");
+  //   // API 호출 후 성공 시 라우팅
+  //   router.push("/login"); // 예시: 로그인 페이지로 이동
+  // };
 
   return (
     <div className="w-full h-full">
       <div className="flex flex-col gap-5 px-5 py-2">
-        <div className="flex justify-center items-center gap-9 px-5">
+        {/* 프로필 */}
+        <div className="flex justify-center items-center gap-9 pt-5">
           <ImageUploader imageUrl={user.imageUrl} onChange={() => {}} />
           <div className="flex flex-col gap-2 items-start">
             <p className="text-text-primary text-lg font-medium">{user.name}</p>
@@ -94,6 +160,27 @@ export default function MyPage() {
               onClick={() => setShowCard(true)}
               className="text-[0.8rem] font-normal px-8"
             />
+            {/* ProfileCardDetail 모달 */}
+            {showCard && (
+              <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4">
+                <div className="relative bg-white mb-12 rounded-xl shadow-lg max-w-sm w-full max-h-[90vh] overflow-y-auto">
+                  {/* 닫기 버튼 */}
+                  <DislikeButton
+                    size="lg"
+                    onClick={() => setShowCard(false)}
+                    className="absolute top-4 right-4 z-10 "
+                    iconClassName="text-hanablack hover:text-gray-500 active:text-gray-900"
+                  />
+                  <ProfileCardDetail
+                    user={user}
+                    answers={answer}
+                    segments={segments}
+                    showDetail={true}
+                    modalView={true}
+                  />
+                </div>
+              </div>
+            )}
           </div>
         </div>
 
@@ -105,38 +192,57 @@ export default function MyPage() {
             values={user.portfolioValues}
             portfolioType={user.portfolioType}
             debtLabel={user.debtPercent}
-            valueFormat="currency"
+            showPercent={true}
           />
         </div>
 
         <MonthlySpendingChart segments={segments} />
       </div>
-      {/* 새롭게 추가될 메뉴 리스트 부분 */}
+      {/* 메뉴 리스트 부분 */}
       <div className="bg-white mt-4 shadow-sm">
-        {menuItems.map((item) => (
-          <div
-            key={item.name}
-            className="flex items-center justify-between px-4 py-4 cursor-pointer text-text-primary text-base font-normal border-t border-gray-200 last:border-b-0"
-            onClick={() => router.push(item.path)}
-          >
-            <span>{item.name}</span>
-            {/* 오른쪽 화살표 아이콘 */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-5 text-gray-400"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m8.25 4.5 7.5 7.5-7.5 7.5"
-              />
-            </svg>
-          </div>
-        ))}
+        {menuItems.map((item) => {
+          const commonClass =
+            "flex items-center justify-between w-full px-4 py-4 cursor-pointer text-text-primary text-base font-normal border-t border-gray-200 last:border-b-0 bg-white";
+
+          if (item.type === "link") {
+            return (
+              <div
+                key={item.name}
+                className={commonClass}
+                onClick={() => router.push(item.path)}
+              >
+                <span>{item.name}</span>
+                <RightArrow size={20} className="text-gray-400" />
+              </div>
+            );
+          }
+
+          if (item.type === "modal") {
+            return (
+              <DialogButton
+                key={item.name}
+                title={item.modalProps.title}
+                content={item.modalProps.content}
+                open={activeDialog === item.name}
+                onClose={() => setActiveDialog(null)}
+                onAction={() => {
+                  item.modalProps.onAction();
+                  setActiveDialog(null);
+                }}
+              >
+                <div
+                  className={commonClass}
+                  onClick={() => setActiveDialog(item.name)}
+                >
+                  <span>{item.name}</span>
+                  <RightArrow size={20} className="text-gray-400" />
+                </div>
+              </DialogButton>
+            );
+          }
+
+          return null;
+        })}
       </div>
     </div>
   );
