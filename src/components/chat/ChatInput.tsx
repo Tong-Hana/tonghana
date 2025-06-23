50090627441199b26c8a123acc9db4c7d07c08eb
jhpark0888 <jhpark0888@naver.com>
2025-06-23 15:03:30 +0900

    [no-issue] fix: 채팅 질문 프리셋 입력창 포커스 됐을 때 나오도록 수정

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,18 +1,19 @@
 "use client";
 
 import { PaperAirplane } from "@/assets/assets";
-import { Ref, useState } from "react";
+import { RefObject, useRef, useState } from "react";
 import ChatPreset from "./ChatPreset";
 import { cn } from "@/utils/cn";
 
 type Props = {
-  inputRef: Ref<HTMLInputElement>;
+  inputRef?: RefObject<HTMLInputElement | null>;
   onSend: (text: string) => void;
 };
 
 export default function ChatInput({ inputRef, onSend }: Props) {
   const [text, setText] = useState("");
   const [showPreset, setShowPreset] = useState(false);
+  const blurTimer = useRef<NodeJS.Timeout | null>(null);
 
   const handleSend = () => {
     if (!text.trim()) return;
@@ -24,7 +25,7 @@ export default function ChatInput({ inputRef, onSend }: Props) {
 
   const handleChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
     setText(event.target.value);
-    setShowPreset(event.target.value.trim().length > 0);
+    setShowPreset(event.target.value.trim().length <= 0);
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -32,6 +33,24 @@ export default function ChatInput({ inputRef, onSend }: Props) {
       e.preventDefault();
       handleSend();
     }
+  };
+
+  const handleSelectPreset = (text: string) => {
+    setText(text);
+    if (inputRef) {
+      inputRef.current?.focus();
+    }
+  };
+
+  const handleFocus = () => {
+    if (blurTimer.current) clearTimeout(blurTimer.current);
+    setShowPreset(text.length <= 0);
+  };
+
+  const handleBlur = () => {
+    blurTimer.current = setTimeout(() => {
+      setShowPreset(false);
+    }, 100);
   };
 
   return (
@@ -44,13 +63,15 @@ export default function ChatInput({ inputRef, onSend }: Props) {
             : "max-h-0 opacity-0 translate-y-40",
         )}
       >
-        <ChatPreset onSelectPreset={setText} />
+        <ChatPreset onSelectPreset={handleSelectPreset} />
       </div>
       <div className="flex w-full px-5 py-3 gap-2 frame-container bg-background items-center">
         <input
           className="h-9 flex-grow rounded-lg bg-white px-3 text-sm text-text-primary border border-hanagreen-normal focus:border-2 focus:outline-none"
           ref={inputRef}
           value={text}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
           onChange={handleChangeText}
           onKeyDown={handleKeyDown}
         />
