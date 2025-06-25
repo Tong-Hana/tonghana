cb3743605a5865b14c5275d141cd4e56de6af787
youlimsongs <giuhki@gmail.com>
2025-06-25 10:35:38 +0900

    [#138] feat: 퀴즈 완료 상태 zustand 설정

--- src/lib/store/userStore.ts
+++ src/lib/store/userStore.ts
@@ -4,10 +4,15 @@ interface UserState {
   nickname: string | null;
   setNickname: (nickname: string) => void;
   clearNickname: () => void;
+  isQuizResolved: boolean;
+  setIsQuizResolved: (isResolved: boolean) => void;
 }
 
 export const useUserStore = create<UserState>((set) => ({
   nickname: null,
   setNickname: (nickname: string) => set({ nickname }),
   clearNickname: () => set({ nickname: null }),
+  isQuizResolved: false,
+  setIsQuizResolved: (isResolved: boolean) =>
+    set({ isQuizResolved: isResolved }),
 }));
