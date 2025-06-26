047f9a463e85a6b339a3a19cc1dca89e144f3a55
zzimnii <happykeil@naver.com>
2025-06-27 00:29:29 +0900

    [#38] ci: EC2 배포 script chat 서버 설정

--- src/lib/socket/client.ts
+++ src/lib/socket/client.ts
@@ -4,7 +4,7 @@ let socket: Socket | null = null;
 
 export const getSocket = () => {
   if (!socket) {
-    socket = io("http://localhost:3001", {
+    socket = io("http://3.35.57.215/:3001", {
       path: "/socket.io",
       transports: ["websocket"],
       autoConnect: false,
