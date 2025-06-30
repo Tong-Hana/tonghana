755396ebea119a49a192fe60ead27a8429ed2d19
박승희 <ridia05031@gmail.com>
2025-06-30 13:54:03 +0900

    [no-issue] style: 퀴즈 페이지 카드 shadow 제거

--- src/app/(navbar)/quiz/page.tsx
+++ src/app/(navbar)/quiz/page.tsx
@@ -12,8 +12,7 @@ import { quizQueryOptions } from "@/hooks/useQuiz";
 export default function QuizPage() {
   const { data } = useSuspenseQuery(quizQueryOptions());
   const quizData = data.subject;
-  const productCardStyle =
-    "flex flex-col gap-5 p-5  rounded-3xl shadow-card-shadow";
+  const productCardStyle = "flex flex-col gap-5 p-5 rounded-3xl";
   const cardTitleStyle = "text-lg font-semibold text-text-primary leading-7";
   const cardTextStyle = "text-base/light leading-6 text-text-primary";
   const cardSpanStyle = "text-lg text-hanagreen-normal font-medium";
@@ -21,24 +20,26 @@ export default function QuizPage() {
     quizData.subjectType === "대출" ? "대출 한도." : "가입 금액.";
   const needDetail = quizData.subjectType !== "금융상식";
   return (
-    <div className="flex flex-col relative w-full p-5 pb-14 overflow-y-scroll gap-8 scrollbar-hide">
-      <div className="font-light text-xl leading-7 tracking-normal text-text-primary">
+    <div className="flex flex-col relative w-full pb-14 overflow-y-scroll gap-8 scrollbar-hide">
+      <div className="font-light mt-4 text-xl leading-7 tracking-normal text-text-primary">
         퀴즈를 풀고 매칭 상대를 더 만나 보세요!
       </div>
-      <InfoCard
-        content={
-          <>
-            오늘은{" "}
-            <span className="text-hanagreen-normal">하나은행 금융 상품</span>{" "}
-            퀴즈에요!
-            <br />
-            아래 정보를 잘 읽고 풀어보세요
-            <br />
-            기회는 단 한번!
-          </>
-        }
-        imageType={"infoStarBoy"}
-      />
+      <div className="px-1">
+        <InfoCard
+          content={
+            <>
+              오늘은{" "}
+              <span className="text-hanagreen-normal">하나은행 금융 상품</span>{" "}
+              퀴즈에요!
+              <br />
+              아래 정보를 잘 읽고 풀어보세요
+              <br />
+              기회는 단 한번!
+            </>
+          }
+          imageType={"infoStarBoy"}
+        />
+      </div>
       <div className={productCardStyle + " bg-hanared-light"}>
         <div className={`${cardTitleStyle} flex items-center gap-2`}>
           <HanaLogo className="w-5 h-5" />
